840a58d36f3b5e548fc6317a6cd357a7a216d450
puffydx <goldenphoenix510@Gmail.com>
2026-08-03 19:46:12 +0300

    Added in getHumanSelection for Human Players, Step 3 complete.

--- rps.js
+++ rps.js
@@ -11,4 +11,22 @@ computerselect = "Rock";
 return computerselect;
 }
 
+function getHumanSelection () {
+const choice = prompt("Pick one! Rock, Paper, or Scissors");
+
+if (choice.toLowerCase() === "rock") {
+    return "Rock";
+}
+else if (choice.toLowerCase() === "paper") {
+    return "Paper";
+}
+else if (choice.toLowerCase() === "scissors") {
+    return "Scissors";
+}
+else {
+    return "Invalid Choice! Please try again.";
+}
+
+}
 console.log(getComputerSelection());
+alert (getHumanSelection());
